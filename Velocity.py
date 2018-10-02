8a56589ba0a2736a9321bcc5ffc496f19e4dc1cd
tt66421 <tt66421@gmail.com>
2018-10-02 12:37:33 +1000

    ２回目のコミット。 １次元から２次元に変更し、列名も引数にした。

--- Velocity.py
+++ Velocity.py
@@ -1,21 +1,31 @@
 
 # coding: utf-8
 
-# In[ ]:
+# In[23]:
 
 import pandas as pd
 import numpy as np
 
-def Velocity(df):
+def Velocity(df, tname, xname, yname):
     # 速度を格納する空のリスト
+    vx = []
+    vy = []
     v = []
     # 速度を格納していく
     for i in range(len(df)-1):
-        v.append( (df['x'].iloc[i+1] - df['x'].iloc[i]) / (df['t'].iloc[i+1] - df['t'].iloc[i]) )
+        dt = df[tname].iloc[i+1] - df[tname].iloc[i]
+        dx = df[xname].iloc[i+1] - df[xname].iloc[i]
+        dy = df[yname].iloc[i+1] - df[yname].iloc[i]
+        vx.append(dx/dt)
+        vy.append(dy/dt)
+        v.append( ((dx**2 + dy**2)**0.5) / dt )
     # 最後の時刻は速度を定義できないので、NaN を入れる
+    vx.append(np.nan)
+    vy.append(np.nan)
     v.append(np.nan)
     # 空のデータフレームを作成して、速度データを代入し、それを返す
-    dfV = pd.DataFrame()
-    dfV['v'] = v
-    return dfV
+    dfVx = pd.DataFrame({'vx': vx})
+    dfVy = pd.DataFrame({'vy': vy})
+    dfV = pd.DataFrame({'v': v})
+    return dfVx, dfVy, dfV
 
